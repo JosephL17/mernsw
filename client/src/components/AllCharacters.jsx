c9e7ecc0cc1d7042c3ad2962a454d8145d398414
Joseph Lauria <joseph.lauria17@gmail.com>
2025-06-30 16:42:57 -0400

    characters needs work

--- client/src/components/AllCharacters.jsx
+++ client/src/components/AllCharacters.jsx
@@ -1,21 +1,31 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 
 const AllCharacters = () => {
   const [characters, setCharacters] = useState([]);
+ 
+ 
+  useEffect(() => {
+    fetchCharacters()
+  }, []);
 
   const fetchCharacters = async() => {
     const data = await fetch("http://localhost:3000/").then(res => res.json());
-    console.log(data)
+    setCharacters(data);
+    // console.log(characters.name);
   }
-
-  fetchCharacters();
-
+  
+  
+  
   return (
-    <>
-      
-    </>
+
+      <div>
+        {characters.map(character => {
+          <a href="">{character.name}</a>
+        })}
+      </div>
+    
   )
 }
 
