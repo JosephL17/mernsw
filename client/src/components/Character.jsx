cd15a768fcba8b490c5b1c8fad9f139c8a4e0317
mpollak <marcellopollak123@gmail.com>
2025-07-01 13:04:27 -0400

    further implementation of Character.jsx

--- client/src/components/Character.jsx
+++ client/src/components/Character.jsx
@@ -1,40 +1,45 @@
 import React from "react";
-import "react-router-dom"
+import { useParams } from "react-router-dom"
+import { useState, useEffect } from "react"
 
-const Character = (prop) => {
 
-    let [characters, setCharacters] = useState([])
+const Character = () => {
 
+    let [character, setCharacter] = useState([])
+    let { id } = useParams()
     useEffect(() => {
   
-      fetch(`http://localhost:3000/api/character/${prop.id}`)
+      fetch(`http://localhost:3000/api/character/${id}`)
         .then((response) => response.json())
-        .then((body) => setCharacters(body))
+        .then((body) => setCharacter(body))
         .catch((err) => {
           throw new Error('Data could not be fetched!');
         });
     }, []);
+
+    console.log(character);
 
     return(
 
         <main>
             <h1 id="name"></h1>
             <section id="generalInfo">
-            <p>Height: <span id="height"></span> cm</p>
-            <p>Mass: <span id="mass"></span> kg</p>
-            <p>Born: <span id="birth_year"></span></p>
+            <p>Height: <span id="height">{character.height}</span> cm</p>
+            <p>Mass: <span id="mass">{character.mass}</span> kg</p>
+            <p>Born: <span id="birth_year">{character.birth_year}</span></p>
             </section>
             <section id="planets">
             <h2>Homeworld</h2>
-            <p><span id="homeworld"></span></p>
+            <p><span id="homeworld">{character.homeworld}</span></p>
             </section>
             <section id="films">
             <h2>Films appeared in</h2>
             <ul></ul>
             </section>
-            </section>
         </main>
     )
 
 }
+
+export default Character
 
